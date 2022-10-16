df2536361f6c92b4552afd5672c5ec8dd65df4e1
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-10-16 12:28:24 -0400

    feat(app): init server side

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import * as middleware from './middleware/index.js'
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ server/middleware/index.js
@@ -0,0 +1,7 @@
+import errorHandlerMiddleware from './errorHandler.js';
+import notFound from './notFound.js';
+
+export {
+  errorHandlerMiddleware,
+  notFound,
+}
